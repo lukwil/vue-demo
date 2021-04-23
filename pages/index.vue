e2e0080e5f7cfdbdd59a40bbc0297d9148686f2c
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2021-04-23 12:28:39 +0200

    changed index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,107 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome {{vorname}}!
         </v-card-title>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <v-text-field
+          v-model="vorname"
+            label="Vorname"
+          ></v-text-field>
+          <v-text-field
+          v-model="nachname"
+            label="Nachname"
+          ></v-text-field>
+
+          <name-output v-if="vollerName.trim()" :nameProp="vollerName"></name-output>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
+          <v-btn color="error" disa @click="openAlert" :disabled="!nachname"> Open Alert </v-btn>
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
         </v-card-actions>
       </v-card>
+
+    </v-col>
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Joke
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+          v-model="search"
+            label="Search"
+            :loading="loading"
+          ></v-text-field>
+        <joke-output v-for="(joke, index) in jokes" :key="jokes[index]" :setup="joke.setup" :punchline="joke.punchline"></joke-output>
+          <!-- <name-output v-if="vollerName.trim()" :nameProp="vollerName"></name-output> -->
+        </v-card-text>
+      </v-card>
+      
     </v-col>
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang="ts">
+import Vue from 'vue'
+import NameOutput from '~/components/NameOutput.vue'
+import JokeOutput from '~/components/JokeOutput.vue'
+import UpdateCounter from '~/components/UpdateCounter.vue'
 
-export default {
+export default Vue.extend({
   components: {
-    Logo,
-    VuetifyLogo,
+   NameOutput,
+   UpdateCounter,
   },
-}
+  data() {
+    return {
+      vorname: '',
+      nachname: '',
+      search: '',
+      jokes: [{setup: '', punchline: ''}],
+      awaitSearch: false,
+      loading: false,
+    }
+  }, 
+  computed: {
+    vollerName(): string {
+      return `${this.vorname} ${this.nachname}`
+    }
+  },
+  watch: {
+    search(searchString: string) {
+      if (!this.awaitSearch) {
+        setTimeout(() => {
+          console.log(`search string: ${searchString}`)
+          this.getDataFromApi(searchString)
+          this.awaitSearch = false
+        }, 1200) // 1.2 sec delay
+      }
+      this.awaitSearch = true
+    },
+  },
+  methods: {
+    openAlert() {
+      this.getDataFromApi('programming')
+      //alert('Test-Alert')
+    },
+    getDataFromApi(searchString: string) {
+      this.loading = true;
+     // https://official-joke-api.appspot.com/jokes/programming/ten
+      const path = `https://official-joke-api.appspot.com/jokes/${searchString}/ten`;
+      this.$axios.$get(path).then((res: any) => {
+        console.log(res)
+        this.jokes = res;
+        this.loading = false;
+      });
+      
+
+    }
+  },
+
+
+})
 </script>
