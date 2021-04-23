7cd2a2a9ba83fc5c40589994ae7522b81572da3b
Lukas Willburger <lukas.willburger@stud.hs-kempten.de>
2021-04-23 12:26:56 +0200

    added example

---
+++ components/JokeOutput.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <v-card elevation="26" color="#385F73"
+            dark>
+    <v-card-title>{{setup}}</v-card-title>
+    <v-card-text>{{punchline}}</v-card-text>
+  </v-card>
+  <v-divider></v-divider>
+</div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    setup: {
+      type: String,
+      required: true,
+    },
+    punchline: {
+      type: String,
+      required: true,
+    }
+  }
+})
+</script>
---
+++ components/NameOutput.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-chip
+      class="ma-2"
+      color="primary"
+    >
+      {{nameProp}}
+    </v-chip>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    nameProp: {
+      type: String,
+      required: true,
+    }
+  }
+})
+</script>
